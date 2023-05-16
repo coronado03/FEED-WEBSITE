723a12b1123e7619819d0c6c6285dc94d0bfcbe0
coronado03 <sebascoronado03@hotmail.com>
2023-05-16 12:49:43 +0200

    Minor change to config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const fs = require('fs');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config({silent:true});
 
 const env = dotenv.parse(fs.readFileSync('.env'));
 

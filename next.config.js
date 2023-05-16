d0c71d2f9d15f45cfd085c7ca52ec3f7555b5f06
coronado03 <sebascoronado03@hotmail.com>
2023-05-16 13:00:30 +0200

    Fix for vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const fs = require('fs');
-const dotenv = require('dotenv').config({silent:true});
-
-const env = dotenv.parse(fs.readFileSync('.env'));
-
-
 const nextConfig = {
   reactStrictMode: true,
-  env: env,
   swcMinify: true,
   images: {
     remotePatterns: [
